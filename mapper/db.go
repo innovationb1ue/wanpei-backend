29ac1e58b2991d5a295b45ced8c526c1501ac128
Jeff Blue <517262600@qq.com>
2022-12-28 18:11:48 +0800

    switch to standalone local db

--- mapper/db.go
+++ mapper/db.go
@@ -11,7 +11,7 @@ type DbConn struct {
 }
 
 func NewDbConn() *DbConn {
-	dsn := "b1ue:TheHardestOne@tcp(host.docker.internal:3306)/wanpei?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "b1ue:TheHardestOne@tcp(127.0.0.1:3306)/wanpei?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB Connection failed. ")
